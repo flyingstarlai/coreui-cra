93b50a93ef5ce916546daff146a72e9b8d488da2
Starlai <flyingstarlai@gmail.com>
2018-02-04 15:08:33 +0800

    add css modules script

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import styles from './Dashboard.module.css';
 
 class Dashboard extends Component {
 
   render() {
     return (
       <div className="animated fadeIn">
-        Hello World
+        <span className={styles.pred}>
+          Hello World
+        </span>
       </div>
     )
   }
